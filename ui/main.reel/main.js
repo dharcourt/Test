129da4796f91ae95aded13731d55b55fcdba22cb
Charles d'Harcourt <noreply>
2015-02-22 22:44:24 +0000

    Play with RangeController content changes

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -3,8 +3,6 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component;
-var Cache = require("logic/cache").Cache;
-var CacheEntry = require("logic/cache-entry").CacheEntry;
 
 /**
  * @class Main
@@ -14,51 +12,28 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     constructor: {
         value: function Main() {
             this.super();
-            this.cache = new Cache(CacheEntry.entriesForProperties([
-                {
-                    data: {},
-                    metadata: {description: "Region1", type: "Media", size: 40},
-                    subEntries: [
-                        {
-                            data: {},
-                            metadata: {description: "Pictures", type: "jpg", size: 15},
-                            subEntries: []
-                        },
-                        {
-                            data: {},
-                            metadata: {description: "Videos", type: "mp4", size: 20},
-                            subEntries: []
-                        },
-                        {
-                            data: {},
-                            metadata: {description: "Information", type: "txt", size: 5},
-                            subEntries: []
-                        }
-                    ]
-                },
-                {
-                    data: {},
-                    metadata: {description: "Region2", type: "Media", size: 60},
-                    subEntries: [
-                        {
-                            data: {},
-                            metadata: {description: "Pictures", type: "jpg", size: 18},
-                            subEntries: []
-                        },
-                        {
-                            data: {},
-                            metadata: {description: "Videos", type: "mp4", size: 40},
-                            subEntries: []
-                        },
-                        {
-                            data: {},
-                            metadata: {description: "Information", type: "txt", size: 2},
-                            subEntries: []
-                        }
-                    ]
-                }
-            ]));
-            console.log(this.cache);
+        }
+    },
+
+    data: {
+        value: [{text: "one"}, {text: "two"}, {text: "three"}]
+    },
+
+    enterDocument: {
+        value: function (firstTime) {
+            var self = this;
+            if (firstTime) {
+                this.templateObjects.rangeController.content = this.data;
+                window.setTimeout(function () {
+                    self.data.push({text: "pushed"});
+                }, 1000);
+                window.setTimeout(function () {
+                    self.data.splice(1, 1);
+                }, 2000);
+                window.setTimeout(function () {
+                    self.data.splice(2, 0, {text: "inserted"});
+                }, 3000);
+            }
         }
     }
 });
