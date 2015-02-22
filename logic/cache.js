27856c7815113fb13d06a38898ed492fdf630cea
Charles d'Harcourt <noreply>
2015-02-22 22:11:01 +0000

    Play with Montage-data Cache & CacheEntry

---
+++ logic/cache.js
@@ -0,0 +1,24 @@
+/**
+ * @module logic/cache
+ * @requires montage/core/core
+ */
+var Montage = require("montage").Montage;
+/**
+ * @class Cache
+ * @extends Montage
+ */
+exports.Cache = Montage.specialize(/** @lends Cache# */ {
+    constructor: {
+        value: function Cache(entries) {
+            this.super();
+            this.entries = entries || [];
+        }
+    },
+
+    /**
+     * @type {CacheEntry[]}
+     */
+    entries: {
+        value: []
+    }
+});
--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -3,6 +3,8 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component;
+var Cache = require("logic/cache").Cache;
+var CacheEntry = require("logic/cache-entry").CacheEntry;
 
 /**
  * @class Main
@@ -12,6 +14,51 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     constructor: {
         value: function Main() {
             this.super();
+            this.cache = new Cache(CacheEntry.entriesForProperties([
+                {
+                    data: {},
+                    metadata: {description: "Region1", type: "Media", size: 40},
+                    subEntries: [
+                        {
+                            data: {},
+                            metadata: {description: "Pictures", type: "jpg", size: 15},
+                            subEntries: []
+                        },
+                        {
+                            data: {},
+                            metadata: {description: "Videos", type: "mp4", size: 20},
+                            subEntries: []
+                        },
+                        {
+                            data: {},
+                            metadata: {description: "Information", type: "txt", size: 5},
+                            subEntries: []
+                        }
+                    ]
+                },
+                {
+                    data: {},
+                    metadata: {description: "Region2", type: "Media", size: 60},
+                    subEntries: [
+                        {
+                            data: {},
+                            metadata: {description: "Pictures", type: "jpg", size: 18},
+                            subEntries: []
+                        },
+                        {
+                            data: {},
+                            metadata: {description: "Videos", type: "mp4", size: 40},
+                            subEntries: []
+                        },
+                        {
+                            data: {},
+                            metadata: {description: "Information", type: "txt", size: 2},
+                            subEntries: []
+                        }
+                    ]
+                }
+            ]));
+            console.log(this.cache);
         }
     }
 });
